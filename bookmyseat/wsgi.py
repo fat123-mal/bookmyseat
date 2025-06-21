2a33133a6054cb24c158d3bd7fbbe5f381698f4c
Fathima <pathuf580@gmail.com>
2025-06-22 00:16:18 +0530

    Add Python version print to wsgi.py for debug

--- bookmyseat/wsgi.py
+++ bookmyseat/wsgi.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+print("Python version:", sys.version)  # <-- ithu add cheyyuka
 
 from django.core.wsgi import get_wsgi_application
 
